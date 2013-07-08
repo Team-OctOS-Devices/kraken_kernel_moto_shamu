fe0a4706dce5e5f2c5507b2eebad0bbca6fc7103
Kyungsik Lee <kyungsik.lee@lge.com>
2013-07-08 16:01:46 -0700

    lib: add support for LZ4-compressed kernel
    Add support for extracting LZ4-compressed kernel images, as well as
    LZ4-compressed ramdisk images in the kernel boot process.
    
    Signed-off-by: Kyungsik Lee <kyungsik.lee@lge.com>
    Cc: "H. Peter Anvin" <hpa@zytor.com>
    Cc: Ingo Molnar <mingo@elte.hu>
    Cc: Thomas Gleixner <tglx@linutronix.de>
    Cc: Russell King <rmk@arm.linux.org.uk>
    Cc: Borislav Petkov <bp@alien8.de>
    Cc: Florian Fainelli <florian@openwrt.org>
    Cc: Yann Collet <yann.collet.73@gmail.com>
    Signed-off-by: Andrew Morton <akpm@linux-foundation.org>
    Signed-off-by: Linus Torvalds <torvalds@linux-foundation.org>
    Signed-off-by: flar2 <asegaert@gmail.com>
    Change-Id: I9c131cf94b27f7c4f32b36455839479b4e7b6003

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,4 +1,5 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 /* This file is auto generated, version 2 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
@@ -13,6 +14,12 @@
 =======
 #define UTS_VERSION "#1 SMP PREEMPT Sun Sep 18 19:12:44 CDT 2016"
 >>>>>>> 0a35ad0... Enable LZ4 Support.
+=======
+/* This file is auto generated, version 2 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 SMP PREEMPT Sun Sep 25 13:48:10 CDT 2016"
+>>>>>>> 049f09a... lib: add support for LZ4-compressed kernel
 #define LINUX_COMPILE_BY "perseus"
 #define LINUX_COMPILE_HOST "lifebook-ubuntu"
 #define LINUX_COMPILER "gcc version 4.8.5 (UBERTC-4.8.5) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,5 +1,6 @@
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 #define UTS_RELEASE "3.10.40-Kraken-g6c56db8-dirty"
 =======
 #define UTS_RELEASE "3.10.40-Kraken-gef7143e-dirty"
@@ -7,3 +8,6 @@
 =======
 #define UTS_RELEASE "3.10.40-Kraken-g56bcf79-dirty"
 >>>>>>> 0a35ad0... Enable LZ4 Support.
+=======
+#define UTS_RELEASE "3.10.40-Kraken-g53f1a28-dirty"
+>>>>>>> 049f09a... lib: add support for LZ4-compressed kernel
