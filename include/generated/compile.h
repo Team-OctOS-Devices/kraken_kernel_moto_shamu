ab29f0a251ae77f4588712308adea4ed8edf5f95
TwistedUmbrella <twistedumbrella@gmail.com>
2014-10-31 18:10:38 -0400

    cpufreq: Initial setup for new gov [Umbrella Core]
    Source - https://github.com/StarKissed/starkissed-kernel-trlte/blob/callofdata/drivers/cpufreq/cpufreq_umbrella_core.c
    Author - twistedumbrella@xda / StarKissed
    
    Change-Id: Ib70d18084240e293b3be1c005b1253ea1673b299

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,14 @@
+<<<<<<< HEAD
 /* This file is auto generated, version 2 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
 #define UTS_VERSION "#2 SMP PREEMPT Fri Sep 16 07:40:03 CDT 2016"
+=======
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Sat Sep 17 18:53:52 CDT 2016"
+>>>>>>> 72fd899... cpufreq: Initial setup for new gov [Umbrella Core]
 #define LINUX_COMPILE_BY "perseus"
 #define LINUX_COMPILE_HOST "lifebook-ubuntu"
 #define LINUX_COMPILER "gcc version 4.8.5 (UBERTC-4.8.5) "
