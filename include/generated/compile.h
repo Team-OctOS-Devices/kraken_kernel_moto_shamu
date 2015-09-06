afefe67797e6ac6731fb8893408eec72d6899afd
jamison904 <james.jamison85@gmail.com>
2015-09-06 11:35:00 -0400

    BioShock Governor.
    Credit - jamison904/android_kernel_samsung_trlte
    Author - jamison904@Xda
    
    Change-Id: I2d91cf49861c594cae8788e51b8330d4d57b7975

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -1,7 +1,7 @@
-/* This file is auto generated, version 1 */
+/* This file is auto generated, version 2 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#1 SMP PREEMPT Thu Sep 15 21:42:56 CDT 2016"
+#define UTS_VERSION "#2 SMP PREEMPT Fri Sep 16 07:40:03 CDT 2016"
 #define LINUX_COMPILE_BY "perseus"
 #define LINUX_COMPILE_HOST "lifebook-ubuntu"
 #define LINUX_COMPILER "gcc version 4.8.5 (UBERTC-4.8.5) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,1 +1,1 @@
-#define UTS_RELEASE "3.10.40-Kraken-g9661570-dirty"
+#define UTS_RELEASE "3.10.40-Kraken-g6c56db8-dirty"
