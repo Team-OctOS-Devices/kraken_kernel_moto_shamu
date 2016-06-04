8424a3fa319db0d4b5af5b3eef14c95d2718ec15
Perseus71 <perseus71@gmail.com>
2016-06-04 18:59:42 -0500

    Enable LZ4 Support.
    Change-Id: If8f0197b27f9cdfc4632c2585d2c7264edd90b3f

--- include/generated/compile.h
+++ include/generated/compile.h
@@ -7,8 +7,12 @@
 /* This file is auto generated, version 1 */
 /* SMP PREEMPT */
 #define UTS_MACHINE "arm"
+<<<<<<< HEAD
 #define UTS_VERSION "#1 SMP PREEMPT Sat Sep 17 18:53:52 CDT 2016"
 >>>>>>> 72fd899... cpufreq: Initial setup for new gov [Umbrella Core]
+=======
+#define UTS_VERSION "#1 SMP PREEMPT Sun Sep 18 19:12:44 CDT 2016"
+>>>>>>> 0a35ad0... Enable LZ4 Support.
 #define LINUX_COMPILE_BY "perseus"
 #define LINUX_COMPILE_HOST "lifebook-ubuntu"
 #define LINUX_COMPILER "gcc version 4.8.5 (UBERTC-4.8.5) "
--- include/generated/utsrelease.h
+++ include/generated/utsrelease.h
@@ -1,5 +1,9 @@
 <<<<<<< HEAD
+<<<<<<< HEAD
 #define UTS_RELEASE "3.10.40-Kraken-g6c56db8-dirty"
 =======
 #define UTS_RELEASE "3.10.40-Kraken-gef7143e-dirty"
 >>>>>>> 72fd899... cpufreq: Initial setup for new gov [Umbrella Core]
+=======
+#define UTS_RELEASE "3.10.40-Kraken-g56bcf79-dirty"
+>>>>>>> 0a35ad0... Enable LZ4 Support.
