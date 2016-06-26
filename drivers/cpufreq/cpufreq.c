1255e202f00562bfa647eba42e07b3dbd70b75ca
perseus <perseus71@gmail.com>
2016-06-26 17:35:28 -0500

    Kraken gets Mean Bite Pt 2.
    Change-Id: Ia93c6178fe32a941e10d21b69e6e1e648138b073

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -638,19 +638,15 @@ static ssize_t show_bios_limit(struct cpufreq_policy *policy, char *buf)
 	return sprintf(buf, "%u\n", policy->cpuinfo.max_freq);
 }
 
+#ifdef CONFIG_MSM_CPU_VOLTAGE_CONTROL
+extern ssize_t show_UV_mV_table(struct cpufreq_policy *policy,
+				char *buf);
 extern ssize_t vc_get_vdd(char *buf);
 extern void vc_set_vdd(const char *buf);
 
-static ssize_t show_UV_mV_table(struct cpufreq_policy *policy, char *buf)
-{
-       return vc_get_vdd(buf);
-}
-static ssize_t store_UV_mV_table
-(struct cpufreq_policy *policy, const char *buf, size_t count)
-{
-       vc_set_vdd(buf);
-       return count;
-}
+extern ssize_t store_UV_mV_table(struct cpufreq_policy *policy,
+				 const char *buf, size_t count);
+#endif
 
 cpufreq_freq_attr_ro_perm(cpuinfo_cur_freq, 0400);
 cpufreq_freq_attr_ro(cpuinfo_min_freq);
