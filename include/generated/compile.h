adf28912ea83a5d239c582bdc6f45e57916f45e3
richard covington <rcovington79@gmail.com>
2016-09-09 17:15:36 -0400

    bring up of N

---
+++ include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1 */
+/* SMP PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#1 SMP PREEMPT Fri Sep 9 16:33:45 EDT 2016"
+#define LINUX_COMPILE_BY "bthorne79"
+#define LINUX_COMPILE_HOST "kraken-box"
+#define LINUX_COMPILER "gcc version 4.8.5 (UBERTC-4.8.5) "
---
+++ include/generated/utsrelease.h
@@ -0,0 +1,1 @@
+#define UTS_RELEASE "3.10.40-ge7d7a0a"
