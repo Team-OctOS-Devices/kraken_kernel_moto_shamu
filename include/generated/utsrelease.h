1bb1bb6e6507d9ae4f3dfe199af54e28df01288d
perseus <perseus71@gmail.com>
2016-12-21 20:44:56 -0600

    Cleanup after commit.
    Change-Id: I797a09d6b941e2c48fce71486080fc66ced1a7ca

--- include/generated/utsrelease.h
+++
@@ -1,5 +0,0 @@
-<<<<<<< HEAD
-#define UTS_RELEASE "3.10.40-Kraken-g6c56db8-dirty"
-=======
-#define UTS_RELEASE "3.10.40-Kraken-gef7143e-dirty"
->>>>>>> 72fd899... cpufreq: Initial setup for new gov [Umbrella Core]
